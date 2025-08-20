36125b180b52da1e2601f13d297bce48ec5754aa
poviraevsergei <Siarhey.Pavirayeu@ventionteams.com>
2025-08-20 21:12:11 +0300

    added for with logic

--- src/lesson3/NumberTask.java
+++ src/lesson3/NumberTask.java
@@ -2,6 +2,8 @@ package lesson3;
 
 public class NumberTask {
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 }
