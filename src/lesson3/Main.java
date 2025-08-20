a7cc5a0c092dba3d613f4b26f44719771faa0c90
poviraevsergei <Siarhey.Pavirayeu@ventionteams.com>
2025-08-20 21:19:57 +0300

    removed logic

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Boolean isAdult = true;
-        boolean x = true;
-
         //Как делать commit? Изменение на Github :)
         // Commit and push
 
